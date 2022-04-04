7234f2e92a658b2041ec923b3c5064fb953cbccd
RamonPapes <ramonpapes@gmail.com>
2022-04-04 11:02:54 -0300

    update curso

---
+++ course-manager/src/app/course-info/course-info.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+
+@Component({
+  selector: 'app-course-info',
+  templateUrl: './course-info.component.html',
+  styleUrls: ['./course-info.component.css']
+})
+export class CourseInfoComponent implements OnInit {
+
+  courseId: number = 0;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.courseId =  + Number(this.activatedRoute.snapshot.paramMap.get('id'));
+  }
+
+}
