b79d350d27a53b7b3c5c843d1738accb1fc481e8
Ramon Papes <90970725+RamonPapes@users.noreply.github.com>
2022-06-06 10:24:00 -0300

    Delete film-list directory

--- film-list/src/app/shared/models/alerta.ts
+++
@@ -1,9 +0,0 @@
-export interface Alerta {
-  titulo?:string;
-  descricao?:string;
-  btnSucesso?:string;
-  btnCancelar?:string;
-  corbtnSucesso?:string;
-  corbtnCancelar?:string;
-  possuirBtnFechar?:boolean;
-}
